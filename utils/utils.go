ee444e1d0f384e804ce1cd0ea45f76ed64d6fc45
Chris Ondrovic <ondrovic@gmail.com>
2024-08-06 14:35:56 -0400

    Utils
    Created some base utils and test to share

---
+++ utils/utils.go
@@ -0,0 +1,202 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	// "github.com/ondrovic/go-shared-lib/consts"
+	// "github.com/ondrovic/go-shared-lib/types"
+	// "github.com/ondrovic/go-shared-lib/vars"
+)
+
+// FileType
+type FileType string
+
+// OperatorType
+type OperatorType string
+
+// SizeUnit struct for units
+type SizeUnit struct {
+	Label string
+	Size  int64
+}
+
+// TestLayout
+type TestLayout[InputT any, ExpectedT any] struct {
+	Name     string
+	Input    InputT
+	Expected ExpectedT
+	Err      error
+}
+
+// CommandExecutor is an interface for executing commands
+type CommandExecutor interface {
+	Run() error
+}
+
+// RealCmd is a wrapper for exec.Cmd that implements CommandExecutor
+type RealCmd struct {
+	cmd *exec.Cmd
+}
+
+// Run executes the command and returns any errors
+func (r *RealCmd) Run() error {
+	r.cmd.Stdout = os.Stdout
+	return r.cmd.Run()
+}
+
+// FileTypes
+const (
+	Any       FileType = "Any"
+	Video     FileType = "Video"
+	Image     FileType = "Image"
+	Archive   FileType = "Archive"
+	Documents FileType = "Documents"
+)
+
+// OperatorTypes
+const (
+	EqualTo            OperatorType = "Equal To"
+	GreaterThan        OperatorType = "Greater Than"
+	GreaterThanEqualTo OperatorType = "Greater Than Or Equal To"
+	LessThan           OperatorType = "Less Than"
+	LessThanEqualTo    OperatorType = "Less Than Or Equal To"
+)
+
+
+var (
+	// SizeUnits
+	SizeUnits = []SizeUnit{
+		{Label: "PB", Size: 1 << 50}, // Petabyte
+		{Label: "TB", Size: 1 << 40}, // Terabyte
+		{Label: "GB", Size: 1 << 30}, // Gigabyte
+		{Label: "MB", Size: 1 << 20}, // Megabyte
+		{Label: "KB", Size: 1 << 10}, // Kilobyte
+		{Label: "B", Size: 1},        // Byte
+	}
+	// FileExtensions
+	FileExtensions = map[FileType]map[string]bool{
+		Any: {
+			"*.*": true,
+		},
+		Video: {
+			".mp4": true, ".avi": true, ".mkv": true, ".mov": true, ".wmv": true,
+			".flv": true, ".webm": true, ".m4v": true, ".mpg": true, ".mpeg": true,
+			".ts": true,
+		},
+		Image: {
+			".jpg": true, ".jpeg": true, ".png": true, ".gif": true, ".bmp": true,
+			".tiff": true, ".webp": true, ".svg": true, ".raw": true, ".heic": true,
+			".ico": true,
+		},
+		Archive: {
+			".zip": true, ".rar": true, ".7z": true, ".tar": true, ".gz": true,
+			".bz2": true, ".xz": true, ".iso": true, ".tgz": true, ".tbz2": true,
+		},
+		Documents: {
+			".docx": true, ".doc": true, ".pdf": true, ".txt": true, ".rtf": true,
+			".odt": true, ".xlsx": true, ".xls": true, ".pptx": true, ".ppt": true,
+			".csv": true, ".md": true, ".pages": true,
+		},
+	}
+
+	execCommand = func(name string, arg ...string) CommandExecutor {
+		return &RealCmd{cmd: exec.Command(name, arg...)}
+	}
+)
+
+// ClearTerminalScreen clears the terminal based on the provided OS name
+func ClearTerminalScreen(goos string) error {
+	var cmd CommandExecutor
+	var err error
+
+	switch strings.ToLower(goos) {
+	case "linux", "darwin":
+		cmd = execCommand("clear")
+	case "windows":
+		cmd = execCommand("cmd", "/c", "cls")
+	default:
+		return fmt.Errorf("unsupported platform: %s", goos)
+	}
+
+	if cmd != nil {
+		err = cmd.Run()
+		if err != nil {
+			fmt.Printf("failed to clear terminal: %s\n", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+// ToFileType
+func ToFileType(fileType string) FileType {
+	switch strings.ToLower(fileType) {
+	case "any":
+		return Any
+	case "video":
+		return Video
+	case "image":
+		return Image
+	case "archive":
+		return Archive
+	case "documents":
+		return Documents
+	default:
+		return ""
+	}
+}
+
+// ToOperatorType
+func ToOperatorType(operatorType string) OperatorType {
+	switch strings.ToLower(operatorType) {
+	case "equal to", "equalto", "equal", "==":
+		return EqualTo
+	case "greater than", "greaterthan", ">":
+		return GreaterThan
+	case "greater than or equal to", "greaterthanorequalto", ">=":
+		return GreaterThanEqualTo
+	case "less than", "lessthan", "<":
+		return LessThan
+	case "less than or equal to", "lessthanorequalto", "<=":
+		return LessThanEqualTo
+	default:
+		return ""
+	}
+}
+
+// FormatSize formats size to human readable
+func FormatSize(bytes int64) string {
+	for _, unit := range SizeUnits {
+		if bytes >= unit.Size {
+			value := float64(bytes) / float64(unit.Size)
+			// Round the value to two decimal places
+			roundedValue := math.Round(value*100) / 100
+			return fmt.Sprintf("%.2f %s", roundedValue, unit.Label)
+		}
+	}
+
+	return "0 B"
+}
+
+// IsExtensionValid checks if the file's extension is allowed for a given file type.
+func IsExtensionValid(fileType FileType, path string) bool {
+	ext := strings.ToLower(filepath.Ext(path))
+	extensions, exists := FileExtensions[fileType]
+	if !exists {
+		return false
+	}
+
+	// Check for wildcard entry (Any)
+	if _, found := extensions["*.*"]; found {
+		return true
+	}
+
+	// Check if the file extension is explicitly allowed
+	return extensions[ext]
+}
---
+++ utils/utils_test.go
@@ -0,0 +1,237 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+)
+
+// MockCmd is a mock implementation of CommandExecutor for simulating errors
+type MockCmd struct {
+	err error
+}
+
+// Run simulates command execution by returning the predefined error
+func (m *MockCmd) Run() error {
+	return m.err
+}
+
+func TestClearTerminalScreen(t *testing.T) {
+	tests := []*TestLayout[string, error]{
+		{Name: "Windows", Input: "windows", Expected: nil},
+		// {Name: "Linux", Input: "linux", Expected: nil},
+		// {Name: "macOS", Input: "darwin", Expected: nil},
+		{Name: "Unsupported OS", Input: "unsupported", Expected: fmt.Errorf("unsupported platform: unsupported")},
+		{Name: "Command Error", Input: "linux", Expected: fmt.Errorf("mock error")},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			// Mock execCommand function based on the test case
+			if test.Name == "Command Error" {
+				execCommand = func(name string, arg ...string) CommandExecutor {
+					return &MockCmd{err: fmt.Errorf("mock error")}
+				}
+			} else {
+				execCommand = func(name string, arg ...string) CommandExecutor {
+					return &RealCmd{cmd: exec.Command(name, arg...)}
+				}
+			}
+
+			result := ClearTerminalScreen(test.Input)
+			if result != nil && test.Expected != nil {
+				if result.Error() != test.Expected.Error() {
+					t.Errorf("ClearTerminalScreen(%q) = %v; expected %v", test.Input, result, test.Expected)
+				}
+			} else if result != test.Expected {
+				t.Errorf("ClearTerminalScreen(%q) = %v; expected %v", test.Input, result, test.Expected)
+			}
+		})
+	}
+}
+
+func TestToFileType(t *testing.T) {
+	tests := []*TestLayout[string, FileType]{
+		{Name: "Test any type", Input: "any", Expected: Any, Err: nil},
+		{Name: "Test video type", Input: "video", Expected: Video, Err: nil},
+		{Name: "Test image type", Input: "image", Expected: Image, Err: nil},
+		{Name: "Test archive type", Input: "archive", Expected: Archive, Err: nil},
+		{Name: "Test documents type", Input: "documents", Expected: Documents, Err: nil},
+		{Name: "Test case-insensitive", Input: "ANY", Expected: Any, Err: nil}, // Case-insensitive check
+		{Name: "Test mixed case", Input: "ViDeO", Expected: Video, Err: nil},   // Mixed case check
+		{Name: "Test invalid input", Input: "invalid", Expected: "", Err: nil}, // Invalid input check
+		{Name: "Test empty input", Input: "", Expected: "", Err: nil},          // Empty string check
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) { // Run each test case as a sub-test
+			result := ToFileType(test.Input)
+			if result != test.Expected {
+				t.Errorf("ToFileType(%q) = %q; expected %q", test.Input, result, test.Expected)
+			}
+		})
+	}
+}
+
+func TestToOperatorType(t *testing.T) {
+	tests := []*TestLayout[string, OperatorType]{
+		{Name: "Test equal to", Input: "equal to", Expected: EqualTo, Err: nil},
+		{Name: `Test equalto`, Input: "equalto", Expected: EqualTo, Err: nil},
+		{Name: "Test equal", Input: "equal", Expected: EqualTo, Err: nil},
+		{Name: "Test ==", Input: "==", Expected: EqualTo, Err: nil},
+
+		{Name: "Test greater than", Input: "greater than", Expected: GreaterThan, Err: nil},
+		{Name: "Test greaterthan", Input: "greaterthan", Expected: GreaterThan, Err: nil},
+		{Name: "Test >", Input: ">", Expected: GreaterThan, Err: nil},
+
+		{Name: "Test greater than or equal to", Input: "greater than or equal to", Expected: GreaterThanEqualTo, Err: nil},
+		{Name: "Test greaterthanorequalto", Input: "greaterthanorequalto", Expected: GreaterThanEqualTo, Err: nil},
+		{Name: "Test >=", Input: ">=", Expected: GreaterThanEqualTo, Err: nil},
+
+		{Name: "Test less than", Input: "less than", Expected: LessThan, Err: nil},
+		{Name: "Test lessthan", Input: "lessthan", Expected: LessThan, Err: nil},
+		{Name: "Test <", Input: "<", Expected: LessThan, Err: nil},
+
+		{Name: "Test less than or equal to", Input: "less than or equal to", Expected: LessThanEqualTo, Err: nil},
+		{Name: "Test lessthanorequalto", Input: "lessthanorequalto", Expected: LessThanEqualTo, Err: nil},
+		{Name: "Test <=", Input: "<=", Expected: LessThanEqualTo, Err: nil},
+
+		{Name: "Test default case", Input: "", Expected: "", Err: nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) { // Run each test case as a sub-test
+			result := ToOperatorType(test.Input)
+			if result != test.Expected {
+				t.Errorf("ToFileType(%q) = %q; expected %q", test.Input, result, test.Expected)
+			}
+		})
+	}
+}
+
+func TestFormatSize(t *testing.T) {
+	tests := []*TestLayout[int64, string]{
+		{Name: "Test 0 B", Input: 0, Expected: "0 B", Err: nil},
+		{Name: "Test 1 B", Input: 1, Expected: "1.00 B", Err: nil},
+		{Name: "Test 2 B", Input: 2, Expected: "2.00 B", Err: nil},
+		{Name: "Test 1 KB", Input: 1024, Expected: "1.00 KB", Err: nil},
+		{Name: "Test 2 KB", Input: 2048, Expected: "2.00 KB", Err: nil},
+		{Name: "Test 1 MB", Input: 1048576, Expected: "1.00 MB", Err: nil},
+		{Name: "Test 2 MB", Input: 2097152, Expected: "2.00 MB", Err: nil},
+		{Name: "Test 1 GB", Input: 1073741824, Expected: "1.00 GB", Err: nil},
+		{Name: "Test 2 GB", Input: 2147483648, Expected: "2.00 GB", Err: nil},
+		{Name: "Test 1 TB", Input: 1099511627776, Expected: "1.00 TB", Err: nil},
+		{Name: "Test 2 TB", Input: 2199023255552, Expected: "2.00 TB", Err: nil},
+		{Name: "Test 1 PB", Input: 1125899906842624, Expected: "1.00 PB", Err: nil},
+		{Name: "Test 2 PB", Input: 2251799813685248, Expected: "2.00 PB", Err: nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) { // Run each test case as a sub-test
+			result := FormatSize(test.Input)
+			if result != test.Expected {
+				t.Errorf("ToFileType(%q) = %q; expected %q", test.Input, result, test.Expected)
+			}
+		})
+	}
+}
+
+func TestIsExtensionValid(t *testing.T) {
+	tests := []*TestLayout[struct {
+		fileType FileType
+		path     string
+	}, bool]{
+		// Tests for Any file type (wildcard)
+		{Name: "Any - valid extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Any, path: "example.file"}, Expected: true},
+		{Name: "Any - no extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Any, path: "example"}, Expected: true},
+
+		// Tests for Video file type
+		{Name: "Video - valid extension .mp4", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Video, path: "video.mp4"}, Expected: true},
+		{Name: "Video - invalid extension .txt", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Video, path: "document.txt"}, Expected: false},
+		{Name: "Video - empty extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Video, path: "video"}, Expected: false},
+
+		// Tests for Image file type
+		{Name: "Image - valid extension .jpg", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Image, path: "picture.jpg"}, Expected: true},
+		{Name: "Image - valid extension .png", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Image, path: "picture.png"}, Expected: true},
+		{Name: "Image - invalid extension .mp4", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Image, path: "video.mp4"}, Expected: false},
+		{Name: "Image - empty extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Image, path: "picture"}, Expected: false},
+
+		// Tests for Archive file type
+		{Name: "Archive - valid extension .zip", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Archive, path: "archive.zip"}, Expected: true},
+		{Name: "Archive - valid extension .tar", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Archive, path: "archive.tar"}, Expected: true},
+		{Name: "Archive - invalid extension .jpg", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Archive, path: "image.jpg"}, Expected: false},
+		{Name: "Archive - empty extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Archive, path: "archive"}, Expected: false},
+
+		// Tests for Documents file type
+		{Name: "Documents - valid extension .pdf", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Documents, path: "document.pdf"}, Expected: true},
+		{Name: "Documents - valid extension .docx", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Documents, path: "document.docx"}, Expected: true},
+		{Name: "Documents - invalid extension .mp4", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Documents, path: "video.mp4"}, Expected: false},
+		{Name: "Documents - empty extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: Documents, path: "document"}, Expected: false},
+
+		// Test for fileType not present in FileExtensions map
+		{Name: "Unknown type - any extension", Input: struct {
+			fileType FileType
+			path     string
+		}{fileType: "unknown_type", path: "file.any"}, Expected: false},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) { // Run each test case as a sub-test
+			result := IsExtensionValid(test.Input.fileType, test.Input.path)
+			if result != test.Expected {
+				t.Errorf("IsExtensionValid(%q, %q) = %v; expected %v", test.Input.fileType, test.Input.path, result, test.Expected)
+			}
+		})
+	}
+}
